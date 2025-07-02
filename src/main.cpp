ebf2ce70e3b233ae8c35f900db7d52f3da6ae4f1
danhkhai07 <aliveomg@gmail.com>
2025-07-02 15:26:04 +0700

    add stop-screen feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "gameManager.h"
 
 #include <iostream>
+#include <limits>
 
 int main(){
     GameManager::Start();
+    // Wait for input before closing
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
     std::cin.get();
     return 0;
 }
